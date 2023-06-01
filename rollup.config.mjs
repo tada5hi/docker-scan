7e0c02cafc89695e790bf2cce6924c9f08e52d1b
tada5hi <peter.placzek1996@gmail.com>
2023-06-01 10:34:22 +0200

    build: use rollup & swc for transpiling and bundling

---
+++ rollup.config.mjs
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2022-2022.
+ * Author Peter Placzek (tada5hi)
+ * For the full copyright and license information,
+ * view the LICENSE file that was distributed with this source code.
+ */
+
+
+import { transform } from "@swc/core";
+import pkg from './package.json' assert {type: 'json'};
+
+export default [
+    {
+        input: './src/index.ts',
+
+        // Specify here external modules which you don't want to include in your bundle (for instance: 'lodash', 'moment' etc.)
+        // https://rollupjs.org/guide/en/#external
+        external: [],
+
+        plugins: [
+            // Compile TypeScript/JavaScript files
+            {
+                name: 'swc',
+                transform(code) {
+                    return transform(code, {
+                        jsc: {
+                            target: 'es2016',
+                            parser: {
+                                syntax: 'typescript'
+                            },
+                            loose: true
+                        },
+                        sourceMaps: true
+                    });
+                }
+            }
+        ],
+        output: [
+            {
+                file: pkg.main,
+                format: 'cjs',
+                sourcemap: true
+            }, {
+                file: pkg.module,
+                format: 'esm',
+                sourcemap: true
+            }
+        ]
+    }
+];
